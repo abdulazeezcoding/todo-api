791dabf44884763509b6ae76a322f3252b9c35ff
abdulazeezcoding <abdulazizmohammed5103@gmail.com>
2024-03-11 14:33:24 +0000

    changes

--- index.js
+++ index.js
@@ -6,18 +6,13 @@ const app = express();
 // Define routes
 
 app.get('/', (req, res)=> {
-    console.log(req.query, req.headers);
+    // console.log(req.query, req.headers);
     res.send("Over and out!");
 });
 
 app.get('/ping', (req, res) => {
-    console.log(req.query, req.headers);
+    // console.log(req.query, req.headers);
     res.send("Ping pong");
-});
-
-app.get('/file', (req, res) => {
-    console.log(process.cwd());
-    res.sendFile(process.cwd() + "/index.html");
 });
 
 // Listen for incoming requests
